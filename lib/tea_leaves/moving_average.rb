10159e9197be67f16571fd3f6f12518e3db6c7d3
Roland Swingler <roland.swingler@gmail.com>
2010-10-24 22:24:28 +0100

    Creating proper methods for simple/weighted MAs

--- lib/tea_leaves/moving_average.rb
+++ lib/tea_leaves/moving_average.rb
@@ -2,29 +2,44 @@ module TeaLeaves
   class MovingAverage
     attr_reader :weights, :number_of_terms
 
-    # Returns a moving average object given a number of terms, a
-    # specialized weighting method symbol or an array of weights.
-    #
-    # 
-    def self.get(average_specifier)
-      if average_specifier.kind_of?(Array)
-        weights = average_specifier
-      elsif average_specifier.kind_of?(Integer)
-        raise ArgumentError.new("Number of terms must be positive") if average_specifier < 1
-        
-        if average_specifier.odd?
-          weights = [1.0 / average_specifier] * (((average_specifier) / 2) + 1)
+    class << self
+      # Returns a moving average object given a number of terms, a
+      # specialized weighting method symbol or an array of weights.
+      #
+      # 
+      def get(average_specifier)
+        if average_specifier.kind_of?(Array)
+          weighted(average_specifier)
+        elsif average_specifier.kind_of?(Integer)
+          simple(average_specifier)
         else
-          weights = [1.0 / average_specifier] * ((average_specifier) / 2) + [1.0 / (2 * average_specifier)]
+          raise ArgumentError.new("Unknown weights")
         end
-      else
-        raise ArgumentError.new("Unknown weights")
+      end
+      
+      def weighted(weights)
+        new expand_weights(weights)
       end
 
-      left_side_weights = weights.clone
-      left_side_weights.shift
+      def simple(n)
+        raise ArgumentError.new("Number of terms must be positive") if n < 1
+        
+        if n.odd?
+          weights = [1.0 / n] * ((n / 2) + 1)
+        else
+          weights = [1.0 / n] * (n / 2) + [1.0 / (2 * n)]
+        end
 
-      new(left_side_weights.reverse + weights)
+        new expand_weights(weights)
+      end
+
+      private
+
+      def expand_weights(weights)
+        left_side_weights = weights.clone
+        left_side_weights.shift
+        left_side_weights.reverse + weights
+      end
     end
 
     def initialize(weights)
--- spec/moving_average_spec.rb
+++ spec/moving_average_spec.rb
@@ -1,43 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TeaLeaves::MovingAverage do
-  it "should raise an ArgumentError if number of terms is < 1" do
-    lambda { MovingAverage.get(0) }.should raise_error(ArgumentError)
-  end
-
-  it "should raise an argument error if given an unknown term specifier" do
-    lambda { MovingAverage.get(:not_real) }.should raise_error(ArgumentError)
-  end
-
   it "should mix a #moving_average method into Array" do
     [0,1,2,6,4].moving_average(3).should == [1,3,4]
   end
 end
 
 describe "a Simple Moving Average" do
+  it "should raise an ArgumentError if number of terms is < 1" do
+    lambda { MovingAverage.simple(0) }.should raise_error(ArgumentError)
+  end
+
   it "should have equal weights with an odd number of terms" do
-    MovingAverage.get(5).weights.should == [0.2, 0.2, 0.2, 0.2, 0.2]
+    MovingAverage.simple(5).weights.should == [0.2, 0.2, 0.2, 0.2, 0.2]
   end
 
   it "should have have half weights at the ends with an even number of terms" do
-    MovingAverage.get(4).weights.should == [0.125, 0.25, 0.25, 0.25, 0.125]
+    MovingAverage.simple(4).weights.should == [0.125, 0.25, 0.25, 0.25, 0.125]
   end
 
   it "should return 3 averages from #calculate with a 3 point MA over 5 terms" do
-    MovingAverage.get(3).calculate([0,1,2,6,4]).should == [1,3,4]
+    MovingAverage.simple(3).calculate([0,1,2,6,4]).should == [1,3,4]
   end
 
   it "should return 1 average from #calculate with a 4 point MA over 5 terms" do
-    MovingAverage.get(4).calculate([0,1,2,6,4]).should == [2.75]
+    MovingAverage.simple(4).calculate([0,1,2,6,4]).should == [2.75]
   end
 
   it "should return no averages from #calculate with a 7 point MA over 5 terms" do
-    MovingAverage.get(7).calculate([0,1,2,6,4]).should == []
+    MovingAverage.simple(7).calculate([0,1,2,6,4]).should == []
   end
 end
 
 describe "a Weighted Moving Average" do
   it "should have symmetric weights when provided with half the list of weights" do
-    MovingAverage.get([0.6, 0.3, 0.1]).weights == [0.1, 0.3, 0.6, 0.3, 0.1]
+    MovingAverage.weighted([0.6, 0.3, 0.1]).weights == [0.1, 0.3, 0.6, 0.3, 0.1]
   end
 end
