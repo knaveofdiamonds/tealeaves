e0577b363dd9e05208cbd6cffbf5f5f40d1cca92
Roland Swingler <roland.swingler@gmail.com>
2012-02-04 13:53:57 +0000

    Improve error checking for weights, and remove symetric cleverness from interface.

--- lib/tealeaves/moving_average.rb
+++ lib/tealeaves/moving_average.rb
@@ -3,63 +3,49 @@ module TeaLeaves
   class MovingAverage
     attr_reader :weights, :number_of_terms
 
-    class << self
-      # Returns a Weighted Moving Average calculator, given a list of
-      # weights. 
-      #
-      # By default the weights are assumed to be symmetric and are
-      # reflected (so you don't need to repeat weights). If symmetric
-      # is false then the weights are assumed to be complete.
-      #
-      # The start of the array of weights is the center weight for the
-      # symmetric MA.
-      #
-      # Examples:
-      #
-      #    MovingAverage.weighted([0.6, 0.2]).weights # => [0.2, 0.6, 0.2]
-      #    MovingAverage.weighted([0.2, 0.7, 0.1], false) # => [0.2, 0.7, 0.1]
-      #
-      def weighted(weights, symmetric=true)
-        new(symmetric ? expand_weights(weights) : weights)
-      end
+    # Returns a Weighted Moving Average calculator, given a list of
+    # weights. 
+    #
+    # The list of weights should be an odd length, and should sum to
+    # 1.
+    #
+    # Examples:
+    #
+    #    MovingAverage.weighted([0.15, 0.7, 0.15]) # => [0.2, 0.7, 0.1]
+    #
+    def self.weighted(weights)
+      new(weights)
+    end
 
-      # Returns a Simple Moving Average calculator, given a number of
-      # terms/points to average over.
-      # 
-      # Examples:
-      #  
-      #    MovingAverage.simple(5).weights # => [0.2, 0.2, 0.2, 0.2, 0.2]
-      # 
-      def simple(n)
-        raise ArgumentError.new("Number of terms must be positive") if n < 1
-        weights = n.odd?() ? [1.0 / n] * n : expand_weights([1.0 / n] * (n / 2) + [1.0 / (2 * n)])
-        new weights
-      end
+    # Returns a Simple Moving Average calculator, given a span n.
+    # 
+    # Examples:
+    #  
+    #    MovingAverage.simple(5).weights # => [0.2, 0.2, 0.2, 0.2, 0.2]
+    #
+    # @param [Integer] n the span or order of the moving average,
+    #   i.e. the number of terms to include in each average.
+    def self.simple(n)
+      raise ArgumentError.new("Number of terms must be positive") if n < 1
+      weights = n.odd?() ? [1.0 / n] * n : expand_weights([1.0 / n] * (n / 2) + [1.0 / (2 * n)])
+      new weights
+    end
 
 
-      # Returns a moving average of a moving average calculator.
-      #
-      # For Example, for a 3x3 MA:
-      # 
-      #     MovingAverage.multiple(3,3).weights #=> [1/9, 2/9, 1/3, 2/9, 1/9]
-      #
-      def multiple(m,n)
-        divisor = (m * n).to_f
-        weights = (1..m).map {|i| i / divisor }
-        num_of_center_weights = ((m + n) / 2) - m
-        num_of_center_weights = 0 if num_of_center_weights < 0
-        num_of_center_weights.times { weights << weights.last }
-
-        new(expand_weights(weights.reverse))
-      end
-
-      private
-
-      def expand_weights(weights)
-        left_side_weights = weights.reverse
-        left_side_weights.pop
-        left_side_weights + weights
-      end
+    # Returns a moving average of a moving average calculator.
+    #
+    # For Example, for a 3x3 MA:
+    # 
+    #     MovingAverage.multiple(3,3).weights #=> [1/9, 2/9, 1/3, 2/9, 1/9]
+    #
+    def self.multiple(m,n)
+      divisor = (m * n).to_f
+      weights = (1..m).map {|i| i / divisor }
+      num_of_center_weights = ((m + n) / 2) - m
+      num_of_center_weights = 0 if num_of_center_weights < 0
+      num_of_center_weights.times { weights << weights.last }
+      
+      new(expand_weights(weights.reverse))
     end
 
     # Creates a new MovingAverage given a list of weights.
@@ -67,8 +53,9 @@ module TeaLeaves
     # See also the class methods simple and weighted.
     def initialize(weights)
       @weights = weights
-      @number_of_terms = @weights.length
-      @each_side = @weights.length / 2
+      @span = @weights.length
+      @each_side = @span / 2
+      check_weights
     end
     
     # Calculates the moving average for the given array of numbers.
@@ -76,7 +63,7 @@ module TeaLeaves
     # Moving averages won't include values for terms at the beginning or end of
     # the array, so there will be fewer numbers than in the original.
     def calculate(array)
-      return [] if number_of_terms > array.length
+      return [] if @span > array.length
       
       (@each_side...(array.length - @each_side)).map do |i|
         array[window(i)].zip(weights).map {|(a,b)| a * b }.inject {|a, b| a + b }
@@ -90,6 +77,21 @@ module TeaLeaves
     def window(i)
       (i - @each_side)..(i + @each_side)
     end
+
+    # Error checking for weights
+    def check_weights
+      raise ArgumentError.new("Weights should be an odd list") unless @span.odd?
+      sum = weights.inject(&:+)
+      if sum < 0.999999 || sum > 1.000001
+        raise ArgumentError.new("Weights must sum to 1")
+      end
+    end
+
+    def self.expand_weights(weights)
+      left_side_weights = weights.reverse
+      left_side_weights.pop
+      left_side_weights + weights
+    end
   end
 
   module ArrayMethods
@@ -100,8 +102,6 @@ module TeaLeaves
     # See MovingAverage for more detail.
     # 
     def moving_average(average_specifier)
-      return self if average_specifier == 1
-
       if average_specifier.kind_of?(Array)
         avg = MovingAverage.weighted(average_specifier)
       elsif average_specifier.kind_of?(Integer)
--- spec/moving_average_spec.rb
+++ spec/moving_average_spec.rb
@@ -1,53 +1,61 @@
 require 'spec_helper'
 
 describe TeaLeaves::MovingAverage do
-  it "should mix a #moving_average method into Array" do
+  it "should mix a #moving_average method into Array that takes a span" do
     [0,1,2,6,4].moving_average(3).should == [1,3,4]
   end
-end
 
-describe "a Simple Moving Average" do
-  it "should raise an ArgumentError if number of terms is < 1" do
-    lambda { TeaLeaves::MovingAverage.simple(0) }.should raise_error(ArgumentError)
-  end
-  
-  it "should have equal weights with an odd number of terms" do
-    TeaLeaves::MovingAverage.simple(5).weights.should == [0.2, 0.2, 0.2, 0.2, 0.2]
-  end
-  
-  it "should have have half weights at the ends with an even number of terms" do
-    TeaLeaves::MovingAverage.simple(4).weights.should == [0.125, 0.25, 0.25, 0.25, 0.125]
+  it "should mix a #moving_average method into Array that takes weights" do
+    [0,1,2,6,4].moving_average([0.2,0.6,0.2]).should == [1.0, 2.6, 4.8]
   end
 
-  it "should return 3 averages from #calculate with a 3 point MA over 5 terms" do
-    TeaLeaves::MovingAverage.simple(3).calculate([0,1,2,6,4]).should == [1,3,4]
+  describe "a Simple Moving Average" do
+    it "should raise an ArgumentError if number of terms is < 1" do
+      lambda { described_class.simple(0) }.should raise_error(ArgumentError)
+    end
+    
+    it "should have equal weights with an odd number of terms" do
+      described_class.simple(5).weights.should == [0.2, 0.2, 0.2, 0.2, 0.2]
+    end
+    
+    it "should have have half weights at the ends with an even number of terms" do
+      described_class.simple(4).weights.should == [0.125, 0.25, 0.25, 0.25, 0.125]
+    end
+    
+    it "should return 3 averages from #calculate with a 3 point MA over 5 terms" do
+      described_class.simple(3).calculate([0,1,2,6,4]).should == [1,3,4]
+    end
+    
+    it "should return 1 average from #calculate with a 4 point MA over 5 terms" do
+      described_class.simple(4).calculate([0,1,2,6,4]).should == [2.75]
+    end
+    
+    it "should return no averages from #calculate with a 7 point MA over 5 terms" do
+      described_class.simple(7).calculate([0,1,2,6,4]).should == []
+    end
   end
 
-  it "should return 1 average from #calculate with a 4 point MA over 5 terms" do
-    TeaLeaves::MovingAverage.simple(4).calculate([0,1,2,6,4]).should == [2.75]
+  describe "a Weighted Moving Average" do
+    it "raises an Argument error if the weights do not sum to 1" do
+      expect { described_class.weighted([0.5, 0.5, 0.1]) }.to raise_error(ArgumentError)
+    end
+
+    it "raises an Argument error if the list of weights is not odd sized" do
+      expect { described_class.weighted([0.5, 0.5]) }.to raise_error(ArgumentError)
+    end
+        
+    it "should allow asymmetric weights" do
+      described_class.weighted([0.6, 0.3, 0.1]).weights == [0.6, 0.3, 0.1]
+    end
   end
 
-  it "should return no averages from #calculate with a 7 point MA over 5 terms" do
-    TeaLeaves::MovingAverage.simple(7).calculate([0,1,2,6,4]).should == []
-  end
-end
-
-describe "a Weighted Moving Average" do
-  it "should have symmetric weights by default when provided with half the list of weights" do
-    TeaLeaves::MovingAverage.weighted([0.6, 0.3, 0.1]).weights == [0.1, 0.3, 0.6, 0.3, 0.1]
-  end
-
-  it "should allow asymmetric weights" do
-    TeaLeaves::MovingAverage.weighted([0.6, 0.3, 0.1], false).weights == [0.6, 0.3, 0.1]
-  end
-end
-
-describe "a Mutliple Moving Average" do
-  it "should combine weights in the 3x3 case" do
-    TeaLeaves::MovingAverage.multiple(3,3).weights.should == [1.0/9, 2.0/9, 3.0/9, 2.0/9, 1.0/9]
-  end
-
-  it "should combine weights in the 3x5 case" do
-    TeaLeaves::MovingAverage.multiple(3,5).weights.should == [1.0/15, 2.0/15, 0.2, 0.2, 0.2, 2.0/15, 1.0/15]
+  describe "a Mutliple Moving Average" do
+    it "should combine weights in the 3x3 case" do
+      described_class.multiple(3,3).weights.should == [1.0/9, 2.0/9, 3.0/9, 2.0/9, 1.0/9]
+    end
+    
+    it "should combine weights in the 3x5 case" do
+      described_class.multiple(3,5).weights.should == [1.0/15, 2.0/15, 0.2, 0.2, 0.2, 2.0/15, 1.0/15]
+    end
   end
 end
